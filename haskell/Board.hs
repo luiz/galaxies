5d78d5a6b2a1443d85513725cf65b40c79cbe52e
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2012-04-28 04:00:12 -0300

    Initial implementation of game board in Haskell

---
+++ haskell/Board.hs
@@ -0,0 +1,87 @@
+module Board where
+
+import qualified Data.Map as Map
+import qualified Data.List as List (intersperse, sortBy, groupBy)
+
+type Position = (Int, Int)
+
+data BallPosition = MidCell Position
+                  | OverEdge Position Position
+
+data Ball = Ball BallPosition
+
+data Cell = Cell { leftEdge :: Bool
+                 , rightEdge :: Bool
+                 , topEdge :: Bool
+                 , bottomEdge :: Bool
+                 }
+
+data Board = Board (Map.Map Position Cell) [Ball]
+
+mkBoard :: Int -> Board
+mkBoard side = Board cells []
+             where
+               mkCellAt x y = Cell (x == 1) (x == side) (y == 1) (y == side)
+               cells = Map.fromList [((x, y), mkCellAt x y) | x <- [1..side], y <- [1..side]]
+
+-- TODO make it work with edges with balls
+showHorizontalEdge :: (Cell -> Bool) -> Cell -> Char
+showHorizontalEdge edge cell = if edge cell
+                               then '—'
+                               else '⋯'
+
+showTopEdges :: [Cell] -> String
+showTopEdges = showEdgesLine topEdge
+
+showLastLine :: [Cell] -> String
+showLastLine = showEdgesLine bottomEdge
+
+showEdgesLine :: (Cell -> Bool) -> [Cell] -> [Char]
+showEdgesLine edge cells = margin ++ edges ++ margin
+                         where
+                           margin = " "
+                           edges = List.intersperse ' ' . map (showHorizontalEdge edge) $ cells
+
+showVerticalEdge :: (Cell -> Bool) -> Cell -> Char
+showVerticalEdge edge cell = if edge cell
+                             then '|'
+                             else '¦'
+
+showFirstEdgeOfLine :: [Cell] -> Char
+showFirstEdgeOfLine = showVerticalEdge leftEdge . head
+
+showCell :: Cell -> String
+showCell cell = ' ' : [showVerticalEdge rightEdge cell]
+
+showCells :: [Cell] -> String
+showCells cells = firstEdge : allButFirstEdge
+                where
+                  firstEdge = showFirstEdgeOfLine cells
+                  allButFirstEdge = foldr (++) "" $ map showCell cells
+
+boardLines :: Board -> [[Cell]]
+boardLines (Board cellMap _) =
+    let
+        keys = Map.keys cellMap
+        sortBySnd pairA pairB = if snd pairA == snd pairB
+                                then fst pairA `compare` fst pairB
+                                else snd pairA `compare` snd pairB
+        keysByLine = List.groupBy (\a b -> snd a == snd b) $ List.sortBy sortBySnd keys
+        unwrapMaybeCell (Just cell) = cell
+        mapBack = unwrapMaybeCell . (flip Map.lookup) cellMap -- guaranteed because we are iterating on the keys
+    in
+        map (map mapBack) keysByLine
+
+instance Show Board where
+    show board =
+        let
+            lines = boardLines board
+            showLine line = showTopEdges line ++ "\n" ++ showCells line
+            allButLastLine = unlines $ map showLine lines
+        in
+            allButLastLine ++ showLastLine (last lines)
+
+-- char to show open vertical edge ¦
+-- char to show closed vertical edge |
+-- char to show open horizontal edge ⋯
+-- char to show closed horizontal edge —
